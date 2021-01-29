7332892efa240f02ab0e646def6dcdfa75a8302f
VoViet <78154687+Bihayho@users.noreply.github.com>
2021-01-29 22:35:42 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CÁC BÀI TẬP VỀ NHÀ PHẦN I</title>
+	<meta charset="utf-8">
+</head>
+<body style="background-color: grey">
+		<h1>Các dạng bài tập đã làm:</h1>
+		<h3>Bài tập 1:</h3>
+		<a href="bai1.html">bai1.1</a>
+		<a href="GalleryOne.html">bai1.2</a>
+		<h3>Bài tập 2:</h3>
+		<a href=""></a>	
+</body>
+</html>
