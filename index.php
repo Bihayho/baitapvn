f622359ff51281e9c6409debe1157f7793db1a98
VoViet <78154687+Bihayho@users.noreply.github.com>
2021-02-23 15:28:54 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,11 +7,14 @@
 <body style="background-color: grey">
 		<h1>Các dạng bài tập đã làm:</h1>
 		<h3>Bài tập 1:</h3>
-		<a href="/baitap1/bai1.html">Bai 1.1</a>
+		<br><a href="/baitap1/bai1.html">Bai 1.1</a>
 		<br><a href="/baitap1/GalleryOne.html">Bai 1.2</a>
 		<h3>Bài tập 2:</h3>
-		<a href="/baitap2/home.html">bai 2</a>
-		<br><h1>Bài tập Advan</h1>
+		<br><a href="/baitap2/home.html">bai 2</a>
+		<br><h3>Bài tập Advan: </h3>
 		<br><a href="baiAdvan/TouristAttractions.html">Bài 1 CSS</a>
+		<br><h3>Bài tập Examination: </h3>
+		<br><a href="baitapTH/baithithuchanh.html">Exam 1</a>
+		<br><a href="baitapTH/baithithuchanh2.html">Exam 2</a>
 </body>
 </html>
